9e324876b63d701dce8533135e98f0969ebf812e
Gardner <gardnerme@reslnvhpc0133.research.chop.edu>
2024-09-19 12:17:01 -0400

    fixing config file generation and correcting OpenNeuro study age

--- code/config_combatLS.sh
+++ code/config_combatLS.sh
@@ -1,17 +1,22 @@
+#!/bin/bash
 #script to write config file that will be used to submit ComBatLS jobs using `subjobs_combatLS.sh`
+#run from outside code dir
 
 #list csvs to be harmonized
-data_path=../data
-config_path=./config_files
+data_path=./data
+config_path=./code/config_files
 
 #batch
 batch="study"
 
 #list covariate effects to preserve
-covar_list=
+covar_list="logAge_days,sexMale,sexMale_x_logAge"
 
-#list desired combat configurations (formula for the above covars)
-combat_list=("y=mx+b" "y ~ pb(x) + b")
+#list desired combat configurations (formula for the above covars - keeping mu and sigma formulas identical for now)
+combat_list='"~pb(logAge_days, method='GAIC', k=log(nrow(df)) + sexMale + pb(sexMale_x_logAge, method='GAIC', k=log(nrow(df))" \
+"~ ns(logAge_days, df=30) + sexMale + ns(sexMale_x_logAge, df=10)" \
+"~ ns(logAge_days, df=20) + sexMale + ns(sexMale_x_logAge, df=5)" \
+"~ ns(logAge_days, knots=c(2.562590, 3.039711, 3.407688, 3.766710, 3.960530, 4.215803, 4.407688)) + sexMale + ns(sexMale_x_logAge, df=10)"'
 
 #make config file dir or remove old file if necessary
 if ! [ -d $config_path ]
@@ -25,13 +30,14 @@ fi
 # create the output file
 touch $config_path/combat_config.txt
 
+
 #loop through each CSV of IDPs separated by phenotype category
-for file in $(find "$data_path" -type f \( -name "*gmv.csv" -o -name "*sa.csv" -o -name "*ct.csv" -o -name "*volglob.csv" -o -name "*sub.csv"\))
+for file in $(find $(realpath $data_path)  -type f -name "*tocombat.csv")
 do
   echo "prepping: $file"
-  for combat_config in combat_list
+  for combat_config in $combat_list
   do
   # Write the CSV file path and the formula to the output file (tab-delimited)
-  echo -e "$file\t$combat_config" >> "$config_path/combat_config.txt"
+  echo -e "$file\t$covar_list\t$combat_config\t$combat_config" >> "$config_path/combat_config.txt"
   done
 done
