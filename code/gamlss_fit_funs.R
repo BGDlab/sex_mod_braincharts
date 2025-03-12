40810f83e0508188a286a3da810e20ecd513f5df
Gardner <gardnerme@reslnvhpc0326.research.chop.edu>
2025-03-12 11:09:58 -0400

    troubleshooting

--- code/gamlss_fit_funs.R
+++ code/gamlss_fit_funs.R
@@ -47,38 +47,38 @@ gamlss_3lambda <- function(pheno, lambda=NULL,
   fs_moment <- match.arg(fs_moment)
   
   #define formulas for each moment
-  mu_base <- paste(
+  mu_base <- paste0(
     "gamlss(formula =", pheno, "~",
     make_pb("sexMale_x_logAge", lambda), "+",
     make_pb("logAge_days", lambda), "+ random(study_site)"
   )
   
-  if (fs_moment == "both" | fs_moment == "mu"){
-    mu_form <- paste(mu_base, "+", fs_ver, ",") #add fs_version term if needed
+  if (fs_moment != "none"){
+    mu_form <- paste0(mu_base, "+", fs_ver, ",") #add fs_version term if needed
   } else {
-    mu_form <- paste(mu_base, ",") #or just comma
+    mu_form <- paste0(mu_base, ",") #or just comma
   }
   
-  sig_base <- paste(
+  sig_base <- paste0(
     "sigma.formula = ~",
     make_pb("sexMale_x_logAge", lambda), "+",
     make_pb("logAge_days", lambda), "+ random(study_site)"
   )
   
-  if (fs_moment == "both") {
-    sig_form <- paste(sig_base, "+", fs_ver, ",")
+  if (fs_moment == "both" | fs_moment == "all") {
+    sig_form <- paste0(sig_base, "+", fs_ver, ",")
   } else {
-    sig_form <- paste(",")
+    sig_form <- paste0(",")
   }
   
   if (fs_moment == "all") {
-    nu_form <- paste("nu.formula = ~", nu_form," + ", fs_ver, ",")
+    nu_form <- paste0("nu.formula = ~", nu_form," + ", fs_ver, ",")
   } else {
-    nu_form <- paste("nu.formula = ~", nu_form,",")
+    nu_form <- paste0("nu.formula = ~", nu_form,",")
   }
   
   if (is.null(start.from)) {
-    control <- paste("control = gamlss.control(n.cyc = 200), family =", fam, ", data= df, trace = FALSE)")
+    control <- paste0("control = gamlss.control(n.cyc = 200), family =", fam, ", data= df, trace = FALSE)")
   } else if (is.gamlss(start.from)) {
     control <- paste0("start.from = ", start.from,
                       "control = gamlss.control(n.cyc = 200), family =", fam,
@@ -120,54 +120,54 @@ gamlss_3lambda <- function(pheno, lambda=NULL,
 #rep model with lambdas from another model obj
 gamlss_3lambda_rep <- function(og_mod, null_mod=TRUE){
   
-  pheno <- paste(og_mod$mu.formula)[2]
+  pheno <- paste0(og_mod$mu.formula)[2]
   fam <- og_mod$family[1]
 
   #define formulas for each moment
   
   #MU
-  mu_base <- paste(og_mod$mu.formula)[3]
+  mu_base <- paste0(og_mod$mu.formula)[3]
   mu_lambdas <- og_mod$mu.lambda
   
   #update lambdas
   if (null_mod == FALSE){
-    mu_base <- sub("sexMale_x_logAge, lambda = *,", paste("sexMale_x_logAge, lambda =", mu_lambdas[1], ","), mu_base)
+    mu_base <- sub("sexMale_x_logAge, lambda = *,", paste0("sexMale_x_logAge, lambda =", mu_lambdas[1], ","), mu_base)
   } else if (null_mod == TRUE) {
     mu_base <- sub("pb\\(sexMale_x_logAge,\\s*lambda\\s*=\\s*[^,]+,\\s*control\\s*=\\s*pb\\.control\\(order\\s*=\\s*3\\)\\)\\s*\\+\\s*", 
                    "", mu_base)
   }
   
-  mu_base <- sub("logAge_days, lambda = *,", paste("logAge_days, lambda =", mu_lambdas[2], ","), mu_base)
-  mu_base <- sub("random(study_site", paste("random(study_site, lambda =", mu_lambdas[3], ")"), mu_base)
+  mu_base <- sub("logAge_days, lambda = *,", paste0("logAge_days, lambda =", mu_lambdas[2], ","), mu_base)
+  mu_base <- sub("random(study_site", paste0("random(study_site, lambda =", mu_lambdas[3], ")"), mu_base)
   
-  mu_form <- paste("gamlss(formula =", pheno, "~", mu_base, ",")
+  mu_form <- paste0("gamlss(formula =", pheno, "~", mu_base, ",")
   
   #SIGMA
-  sig_base <- paste(og_mod$sigma.formula)[2]
+  sig_base <- paste0(og_mod$sigma.formula)[2]
   sig_lambdas <- og_mod$sigma.lambda
   
   #update lambdas
   if (null_mod == FALSE){
-    sig_base <- sub("sexMale_x_logAge, lambda = *,", paste("sexMale_x_logAge, lambda =", sig_lambdas[1], ","), sig_base)
+    sig_base <- sub("sexMale_x_logAge, lambda = *,", paste0("sexMale_x_logAge, lambda =", sig_lambdas[1], ","), sig_base)
   } else if (null_mod == TRUE) {
     sig_base <- sub("pb\\(sexMale_x_logAge,\\s*lambda\\s*=\\s*[^,]+,\\s*control\\s*=\\s*pb\\.control\\(order\\s*=\\s*3\\)\\)\\s*\\+\\s*", 
                    "", sig_base)
   }
-  sig_base <- sub("logAge_days, lambda = *,", paste("logAge_days, lambda =", sig_lambdas[2], ","), sig_base)
-  sig_base <- sub("random(study_site", paste("random(study_site, lambda =", sig_lambdas[3], ")"), sig_base)
+  sig_base <- sub("logAge_days, lambda = *,", paste0("logAge_days, lambda =", sig_lambdas[2], ","), sig_base)
+  sig_base <- sub("random(study_site", paste0("random(study_site, lambda =", sig_lambdas[3], ")"), sig_base)
   
-  sig_form <- paste("sigma.formula = ~", sig_base, ",")
+  sig_form <- paste0("sigma.formula = ~", sig_base, ",")
   
   #NU
-  nu_base <- paste(og_mod$sigma.formula)[2]
-  nu_form <- paste("nu.formula = ~", nu_base, ",")
+  nu_base <- paste0(og_mod$sigma.formula)[2]
+  nu_form <- paste0("nu.formula = ~", nu_base, ",")
   
-  control <- paste("control = gamlss.control(n.cyc = 200), family =", fam, ", data= df, trace = FALSE)")
+  control <- paste0("control = gamlss.control(n.cyc = 200), family =", fam, ", data= df, trace = FALSE)")
   
   #try methods
   
   result <- tryCatch({
-    gamlss_RSformula <-paste(mu_form, sig_form, nu_form, control)
+    gamlss_RSformula <-paste0(mu_form, sig_form, nu_form, control)
     
     eval(parse(text = gamlss_RSformula))
     
@@ -178,7 +178,7 @@ gamlss_3lambda_rep <- function(og_mod, null_mod=TRUE){
   } , error = function(e) {
     message(e$message, ", trying method=CG()")
     tryCatch({
-      gamlss_CGformula <-paste(mu_form, sig_form, nu_form, "method=CG()", control)
+      gamlss_CGformula <-paste0(mu_form, sig_form, nu_form, "method=CG()", control)
       eval(parse(text = gamlss_CGformula))
       
       #if CG also fails, return NULL
