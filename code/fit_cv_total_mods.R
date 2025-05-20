891ea1600aa5f4b3c49130bf2600fbe7697ab965
Margaret Gardner <90057023+megardn@users.noreply.github.com>
2025-05-20 08:41:32 -0400

    remove lambada param

--- code/fit_cv_total_mods.R
+++ code/fit_cv_total_mods.R
@@ -99,14 +99,12 @@ for (fs_include in moment_list){
     for (nu in nu_list){
     nu_name <- names(nu_list)[nu_list==nu]
     
-    print(paste("fitting model with lambda =", l, 
-                ", fs in", fs_include, 
+    print(paste("fitting model with fs in", fs_include, 
                 ", total in", total_include, 
                 "and nu = ", nu_name))
     
     #FIT BASIC MODEL
     model <- gamlss_3lambda_etiv(pheno,
-                            lambda=l,
                             total_var=total, total_moment=total_include,
                             fs_ver=fs, fs_moment=fs_include, 
                             fam="BCCG",
@@ -124,7 +122,7 @@ for (fs_include in moment_list){
     m_name <- paste(paste0("fs", fs_include), nu_name, paste0("total", total_include), sep="_")
     mod_list[[m_name]] <- model
     
-    saveRDS(model, file=paste0(save_path, "/model_objs/", pheno, "_lambda", l.name, "_", m_name, "_mod.rds"))
+    saveRDS(model, file=paste0(save_path, "/model_objs/", pheno, "_", m_name, "_mod.rds"))
   
    
     #COMPILE
@@ -132,7 +130,6 @@ for (fs_include in moment_list){
       tmp_df <- data.frame(
         "AIC" = model$aic,
         "BIC" = model$sbc,
-        "lambda" = l.name,
         "pheno" = pheno,
         "fs_moment" = fs_include,
         "total_moment" = total_include,
@@ -149,7 +146,7 @@ stopifnot(length(mod_list) > 0)
 
 #SAVE CSVs
 print("saving csvs")
-fwrite(summary_df, file=paste0(save_path, "/model_sums/", pheno, "_lambda", l.name, "_summary.csv"))
+fwrite(summary_df, file=paste0(save_path, "/model_sums/", pheno, "_summary.csv"))
 
 print("finding lowest BIC")
 best_bic <- summary_df %>%
@@ -166,8 +163,8 @@ print(best_bic$m_name)
 best_mod <- mod_list[[best_bic$m_name]]
 
 #RENAME BEST MOD
-file.rename(paste0(save_path, "/model_objs/", best_bic$pheno, "_lambda", best_bic$lambda, "_", best_bic$m_name, "_mod.rds"),
-            paste0(save_path, "/model_objs/", best_bic$pheno, "_lambda", best_bic$lambda, "_", best_bic$m_name, "_BestMod.rds"))
+file.rename(paste0(save_path, "/model_objs/", best_bic$pheno, "_", best_bic$m_name, "_mod.rds"),
+            paste0(save_path, "/model_objs/", best_bic$pheno, "_", best_bic$m_name, "_BestMod.rds"))
 
 print("compiling stats")
 
@@ -185,11 +182,11 @@ fan_plot <- make_centile_fan(gamlssModel=best_mod, df=df, x_var="logAge_days", c
                              remove_cent_effect="study_site",
                              remove_point_effect = c(total, "study_site")
                              y_scale=unscale) +
-  labs(title=paste(pheno, "\nsmoothed w/ lamda=", best_bic$lambda, ",", best_bic$m_name),
+  labs(title=paste(pheno, ",", best_bic$m_name),
      x ="log Age (days)",
      color = "Sex=Male", fill="Sex=Male")
 
-ggsave(file=paste0(save_path, "/centile_plots/", pheno, "_lambda", best_bic$lambda, "_", best_bic$m_name, ".png"), fan_plot)
+ggsave(file=paste0(save_path, "/centile_plots/", pheno, "_", best_bic$m_name, ".png"), fan_plot)
 
 fan_plot <- make_centile_fan(gamlssModel=best_mod, df=df, x_var=total, color_var="sexMale",
                              get_peaks=FALSE, desiredCentiles=c(0.05, 0.25, 0.5, 0.75, 0.95),
@@ -198,27 +195,25 @@ fan_plot <- make_centile_fan(gamlssModel=best_mod, df=df, x_var=total, color_var
                              remove_point_effect = c("logAge_days", "study_site"),
 			     y_scale=unscale
                              ) +
-  labs(title=paste(pheno, "\nsmoothed w/ lamda=", best_bic$lambda, ",", best_bic$m_name),
+  labs(title=paste(pheno, ",", best_bic$m_name),
        x = total,
        color = "Sex=Male", fill="Sex=Male")
 
-ggsave(file=paste0(save_path, "/centile_plots/", pheno, "_lambda", best_bic$lambda, "_", best_bic$m_name, "total.png"), fan_plot)
+ggsave(file=paste0(save_path, "/centile_plots/", pheno, "_", best_bic$m_name, "total.png"), fan_plot)
 
 
 #WORM PLOT
 print("creating worm plot")
-wp <- wp.taki(xvar=df$logAge_days, resid=resid(best_mod), n.inter=8) +
-  ggtitle(paste(pheno, "\nsmoothed w/ lambda=", l.name))
-ggsave(file=paste0(save_path, "/worm_plots/", pheno, "_lambda", best_bic$lambda, "_", best_bic$m_name, ".png"), wp)
+wp <- wp.taki(xvar=df$logAge_days, resid=resid(best_mod), n.inter=8)
+ggsave(file=paste0(save_path, "/worm_plots/", pheno, "_", best_bic$m_name, ".png"), wp)
 
 #centiles
 results_df <- cent_cdf(best_mod, df, plot=FALSE, group="sexMale")
-results_df$lambda <- best_bic$lambda
 results_df$fs <- best_bic$fs_moment
 results_df$total <- best_bic$total_moment
 results_df$nu <- best_bic$nu
 
 #centiles
-fwrite(results_df, file=paste0(save_path, "/cent_csvs/", pheno, "_lambda", best_bic$lambda, "_", best_bic$m_name, "_results.csv"))
+fwrite(results_df, file=paste0(save_path, "/cent_csvs/", pheno, "_", best_bic$m_name, "_results.csv"))
 
 print("DONE")
