63fb8e6ea228ae8a5a8e8c82873054ec5438ac3a
Margaret Gardner <mgardner457@gmail.com>
2025-09-15 14:13:33 -0400

    troubeshooting arg parsing, adding mem

--- code/subjobs_gamlss2_mods.sh
+++ code/subjobs_gamlss2_mods.sh
@@ -4,8 +4,8 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=2
-#SBATCH --mem-per-cpu=2G
-#SBATCH --array=1-40
+#SBATCH --mem-per-cpu=4G
+#SBATCH --array=24,40
 #SBATCH --output=/mnt/isilon/bgdlab_processing/Margaret/sex_mod_braincharts/code/jobfiles/R-%A_%a.out
 #SBATCH --error=/mnt/isilon/bgdlab_processing/Margaret/sex_mod_braincharts/code/jobfiles/R-%A_%a.err
 
@@ -16,9 +16,14 @@ echo "Config file: $CONFIGFN"
 echo "SLURM_ARRAY_TASK_ID: $SLURM_ARRAY_TASK_ID"
 
 #PARSE CONFIG FILE
-PHENO=$(awk -F'\t' -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $2}' "$CONFIGFN")
-FORM=$(awk -F'\t' -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $3}' "$CONFIGFN")
-NAME=$(awk -F'\t' -v ArrayTaskID=$SLURM_ARRAY_TASK_ID '$1==ArrayTaskID {print $4}' "$CONFIGFN")
+PHENO=$(awk -F'\t' -v ArrayTaskID=$SLURM_ARRAY_TASK_ID \
+    '$1==ArrayTaskID {print $2}' "$CONFIGFN" | tr -d '\r' | xargs)
+
+FORM=$(awk -F'\t' -v ArrayTaskID=$SLURM_ARRAY_TASK_ID \
+    '$1==ArrayTaskID {print $3}' "$CONFIGFN" | tr -d '\r' | xargs)
+
+NAME=$(awk -F'\t' -v ArrayTaskID=$SLURM_ARRAY_TASK_ID \
+    '$1==ArrayTaskID {print $4}' "$CONFIGFN" | tr -d '\r' | xargs)
 
 echo "PHENO: $PHENO"
 echo "FORM: $FORM"
@@ -26,7 +31,7 @@ echo "NAME: $NAME"
 
 #------------------
 
-SINGULARITY_IMAGE="$BASE/containers/r_gamlss_0.2.2.sif"
+SINGULARITY_IMAGE="$BASE/containers/r_gamlss_0.2.3.sif"
 
 script=$BASE/code/test_gamlss2.R
 
