3de9a17d1ebce3ccb4db31389e400834c8cbc50d
Margaret Gardner <mgardner457@gmail.com>
2026-04-29 15:04:09 -0400

    code to remove test models needing to be rerun

---
+++ rm_sczglu_rerun_test_mods.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+PREFIX_FILE="sczglu_pheno_rerun.txt"
+
+# Read prefixes into an array (strips any trailing whitespace/carriage returns)
+mapfile -t prefixes < <(tr -d '\r' < "$PREFIX_FILE")
+
+found=0
+
+for dir in cv_sample_?_test/*/model_objs/; do
+    [ -d "$dir" ] || continue
+
+    subdir=$(basename "$(dirname "$dir")")
+
+    # Only process subdirs starting with the three target strings
+    if [[ "$subdir" != cortical_vols* && "$subdir" != global_vols* && "$subdir" != subcortical* ]]; then
+        continue
+    fi
+
+    for file in "$dir"*; do
+        [ -f "$file" ] || continue
+        fname=$(basename "$file")
+
+        for prefix in "${prefixes[@]}"; do
+            if [[ "$fname" == "$prefix"* ]]; then
+                rm "$file"
+                ((found++))
+                break
+            fi
+        done
+    done
+done
+
+echo ""
+echo "Total files that would be removed: $found"
